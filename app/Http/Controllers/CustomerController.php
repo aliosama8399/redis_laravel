a4c1bdd548a6afda3d19fd88e0ad85882db3808a
Ali <aliosama8399@gmail.com>
2022-01-24 17:58:24 +0200

    create and update with cache command

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,26 +4,45 @@ namespace App\Http\Controllers;
 
 use App\Models\Customer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Redis;
 
 class CustomerController extends Controller
 {
     public function checkCustomer(Request $request)
     {
-        $customer = Customer::where('national_id',$request->national_id)->first();
+//        $customer = Customer::where('national_id',$request->national_id)->first();
 
-        $national_id = Redis::get('national_'.$request->national_id);
-        info($national_id);
-//
-//    if ($customer){
-//        info('update');
-//        $customer->update($request->all());
-//
-//    }else{
-//        info('update');
-//
-//        Customer::create($request->all());
-//    }
+        $customer_id = Redis::get('national_'.$request->national_id);
+        info($customer_id);
+
+    if ($customer_id){
+        Customer::where('id',$customer_id)->update($request->all());
+
+    }else{
+
+        Customer::create($request->all());
+    }
+
+
+
+
+
+
+    }
+    public function checkCustomerCache(Request $request)
+    {
+//        $customer = Customer::where('national_id',$request->national_id)->first();
+
+        $customer_id=Cache::get('national_id_'.$request->national_id);
+
+        if ($customer_id){
+            Customer::where('id',$customer_id)->update($request->all());
+
+        }else{
+
+            Customer::create($request->all());
+        }
 
 
 
